2b3d5bd6e51973fa230f9bddb764a90e7ed68246
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-04-10 01:15:15 +0900

    Added desktop home page

--- components/button.jsx
+++ components/button.jsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 
 const Button = ({ variant = "primary", text, onClick, className }) => {
   return (
-    <button className={clsx("w-full", className)} onClick={onClick}>
+    <button className={clsx("w-full cursor-pointer", className)} onClick={onClick}>
       <div className="relative w-full h-[74px] lg:h-[130px] flex items-center justify-center">
         <Image src={`/button-${variant}.png`} alt="" fill />
         <div className="z-10 text-[26px] lg:text-[54px] text-[#E9E9E3]">{text}</div>
