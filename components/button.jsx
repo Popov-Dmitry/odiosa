54d611fc9d69cd7fdfd688252d93a7271dc6dd65
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-04-15 20:59:39 +0900

    Added text stroke on button text

--- components/button.jsx
+++ components/button.jsx
@@ -7,7 +7,12 @@ const Button = ({ variant = "primary", text, onClick, className, imageClassName 
     <button className={clsx("w-full cursor-pointer h-[74px] lg:h-[130px] text-[26px] lg:text-[54px]", className)} onClick={onClick}>
       <div className={clsx("relative w-full h-[74px] lg:h-[130px] flex items-center justify-center", imageClassName)}>
         <Image src={`/button-${variant}.png`} alt="" fill />
-        <div className="z-10 text-[#E9E9E3]">{text}</div>
+        <div
+          className="z-10 text-[#E9E9E3]"
+          style={{ WebkitTextStroke: "4px #FFFFFF2E" }}
+        >
+          {text}
+        </div>
       </div>
     </button>
   );
