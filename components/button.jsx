f555fbcd5ba9fb0e9093fa543bc566aed4f6559c
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-04-17 23:19:31 +0900

    Added Contact the manager block

--- components/button.jsx
+++ components/button.jsx
@@ -4,11 +4,14 @@ import clsx from "clsx";
 
 const Button = ({ variant = "primary", text, onClick, className, imageClassName }) => {
   return (
-    <button className={clsx("w-full cursor-pointer h-[74px] lg:h-[130px] text-[26px] lg:text-[54px]", className)} onClick={onClick}>
+    <button
+      className={clsx("w-full cursor-pointer h-[74px] lg:h-[130px] text-[26px] lg:text-[54px]", className)}
+      onClick={onClick}
+    >
       <div className={clsx("relative w-full h-[74px] lg:h-[130px] flex items-center justify-center", imageClassName)}>
         <Image src={`/button-${variant}.webp`} alt="" fill />
         <div
-          className={clsx("z-10", variant === "outlined" ? "text-[#575757] text-glow-20" : "text-[#E9E9E3]")}
+          className={clsx("z-10 uppercase", variant === "outlined" ? "text-[#575757] text-glow-20" : "text-[#E9E9E3]")}
           style={{ WebkitTextStroke: variant === "outlined" ? undefined : "4px #FFFFFF2E" }}
         >
           {text}
--- components/input.jsx
+++ components/input.jsx
@@ -5,20 +5,25 @@ import useResponsive from "@/hooks/use-responsive";
 import Image from "next/image";
 import clsx from "clsx";
 
-const Input = ({ value, onChange, placeholder, disabled, className }) => {
+const Input = ({ variant, value, onChange, placeholder, disabled, textCenter, className, inputClassName }) => {
   const { isMobile } = useResponsive();
 
   return (
     <div className={className}>
       <input
-        className={clsx("w-full outline-0 placeholder:opacity-100", isMobile ? "text-xl text-center" : "text-2xl")}
+        className={clsx(
+          "w-full outline-0 placeholder:opacity-100",
+          isMobile ? "text-xl" : "text-2xl",
+          (isMobile || textCenter) && "text-center",
+          inputClassName
+        )}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
         disabled={disabled}
       />
       <div className={clsx("relative w-full mt-2.5", isMobile ? "h-[9px]" : "h-1.5")}>
-        <Image src={isMobile ? "/input-mobile.webp" : "/input-desktop.webp"} alt="" fill />
+        <Image src={variant ? `/input-${variant}.webp` : isMobile ? "/input-mobile.webp" : "/input-desktop.webp"} alt="" fill />
       </div>
     </div>
   );
