eaefb2676416365169ac6cd000b04517ece36353
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-05-07 19:08:29 +0900

    Added button link

--- components/button.jsx
+++ components/button.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Image from "next/image";
 import clsx from "clsx";
+import Link from "next/link";
 
-const Button = ({ variant = "primary", text, onClick, className, imageClassName }) => {
+const Button = ({ variant = "primary", text, onClick, className, imageClassName, href }) => {
+  if (href) {
+    return (
+      <Link
+        className={clsx("w-full cursor-pointer h-[74px] !no-underline lg:h-[130px] text-[26px] lg:text-[54px]", className)}
+        href={href}
+      >
+        <div className={clsx("relative w-full h-[74px] lg:h-[130px] flex items-center justify-center", imageClassName)}>
+          <Image src={`/button-${variant}.webp`} alt="" fill />
+          <div
+            className={clsx("z-10 uppercase", variant === "outlined" ? "text-[#575757] text-glow-20" : "text-[#E9E9E3]")}
+            style={{ WebkitTextStroke: variant === "outlined" ? undefined : "4px #FFFFFF2E" }}
+          >
+            {text}
+          </div>
+        </div>
+      </Link>
+    );
+  }
+
   return (
     <button
       className={clsx("w-full cursor-pointer h-[74px] lg:h-[130px] text-[26px] lg:text-[54px]", className)}
