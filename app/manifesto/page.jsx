0bdcb1d22084502531c19f27fdd939cdc766d3e8
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-05-12 19:19:34 +0900

    Added border on manifesto

--- app/manifesto/page.jsx
+++ app/manifesto/page.jsx
@@ -2,122 +2,129 @@ import React from "react";
 
 const Manifesto = () => {
   return (
-    <div className="pl-18 pr-5 pb-5 mt-10 flex flex-col gap-20 lg:max-w-[1522px] lg:px-20 lg:mt-20 lg:justify-self-end lg:gap-10 relative">
-      <div className="absolute bottom-0 left-0 right-0 w-full h-[1300px] bg-gradient-to-b from-[#E9E9E500] to-[#E4E4E1] pointer-events-none" />
-      <div className="max-w-[270px] text-[26px] uppercase text-glow-15 self-end lg:max-w-none lg:text-[54px] lg:text-glow-25 lg:self-start">
-        <div className="hidden lg:block">Odiosa</div>
-        Manifesto of the Gentle and Sweet Bastard.
-        <br />
-        An Ode to the Outrageous.
+    <div className="relative lg:max-w-[1522px] lg:flex lg:justify-self-end mt-10 overflow-hidden">
+      <div className="w-[calc(100%_+_300px)] lg:w-full h-full absolute z-10 pointer-events-none pl-2 lg:pl-4 lg:pr-7">
+        <img src="./list.svg" alt="" className="h-full w-full" />
       </div>
-      <ol className="list-decimal flex flex-col gap-6 text-xl text-glow-10 lg:text-[44px] lg:text-glow-25 lg:gap-0">
-        <li className="pl-5 lg:pl-[23px]">
-          Go forth, play, and bring yourself into being.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Do what you fear, in the way that makes you ashamed.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          What is rejected deserves the most attention.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Take steady aim at what’s familiar.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Hold a gaze longer than acceptable.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Awaken yourself into a wild fervor.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Refuse to choose. (When offered a choice, the mind begins a game of narrowing reality and making decisions. Catch this game like a venomous viper, toss it into the woods, and let it run free. Respond randomly but sincerely, upsetting the structures you encounter along the way.)
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Protect those you love and the love itself.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          If it’s dumb, it’s brilliant.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Yes, it’s terrifying.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          "Is it possible to…?" - It always is.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Those with no limits are tested by their own hypotheses. Don’t skimp on safety nets; take care of your health, you rebel!
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Read books. So far, no one’s invented a better way to balance the psyche. It seems you’re a daring one, so compensate for the stress.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          You know nothing about other people — this is the only thing worth accepting.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Don’t let anyone, including yourself, praise or scold you. You’re not “great”; you’re a mix of chromosomes in a smoothie of chance, your parents, luck, and your agreements or disagreements. Don’t change that. Play, fulfill yourself, and don’t overrate the influence of ego.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Creativity, in 99% of cases, is just the excretions of unhinged people. Don’t glorify art.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          There is nothing more valuable than the act of creation. Art is the thing you must climb the altar for, with everything in your pockets, your heart, and in the “forbidden” folder.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Death can become the center of your thoughts, turning you into a vortex swirling around nothingness, endlessly passing yourself through it. You know nothing about death, and this is the only thing worth accepting.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Individuality is nonsense. That’s how our species works; we look at each other, listen to each other, and are obsessed with our own kind. If it weren’t for the God of Chaos, we would have long since become indistinguishable from each other. Don’t stress over it.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          If you’re going to lie, do it extravagantly, in full, with richness. Petty, lousy half-lies lead to dementia and early senility.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Gratitude turns a rebel into a noble, disarming rascal with a purity in their sociopathic motives.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Gratitude — genuine, ringing, and clear — is the ultimate secret weapon.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          The more people and events you are grateful for, the better you cope with everything. Praises to all that is.
-        </li>
-        <div className="pl-5 -mb-6 lg:pl-[23px] lg:text-[54px] lg:mb-0 lg:mt-16">
-          EXTREME ZONE...
+      <div className="px-10">
+        <div className="relative pl-14 pr-7 pb-20 mt-10 flex flex-col gap-20 lg:px-24 lg:max-w-[1522px] lg:mt-20 lg:pb-26 lg:justify-self-end lg:gap-10">
+          <div className="absolute bottom-0 left-0 right-0 w-full h-[1300px] bg-gradient-to-b from-[#E9E9E500] to-[#E4E4E1] pointer-events-none" />
+          <div className="max-w-[270px] text-[26px] uppercase text-glow-15 self-end lg:max-w-none lg:text-[54px] lg:text-glow-25 lg:self-start">
+            <div className="hidden lg:block">Odiosa</div>
+            Manifesto of the Gentle and Sweet Bastard.
+            <br />
+            An Ode to the Outrageous.
+          </div>
+          <ol className="list-decimal flex flex-col gap-6 text-xl text-glow-10 lg:text-[44px] lg:text-glow-25 lg:gap-0">
+            <li className="pl-5 lg:pl-[23px]">
+              Go forth, play, and bring yourself into being.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Do what you fear, in the way that makes you ashamed.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              What is rejected deserves the most attention.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Take steady aim at what’s familiar.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Hold a gaze longer than acceptable.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Awaken yourself into a wild fervor.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Refuse to choose. (When offered a choice, the mind begins a game of narrowing reality and making decisions. Catch this game like a venomous viper, toss it into the woods, and let it run free. Respond randomly but sincerely, upsetting the structures you encounter along the way.)
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Protect those you love and the love itself.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              If it’s dumb, it’s brilliant.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Yes, it’s terrifying.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              "Is it possible to…?" - It always is.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Those with no limits are tested by their own hypotheses. Don’t skimp on safety nets; take care of your health, you rebel!
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Read books. So far, no one’s invented a better way to balance the psyche. It seems you’re a daring one, so compensate for the stress.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              You know nothing about other people — this is the only thing worth accepting.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Don’t let anyone, including yourself, praise or scold you. You’re not “great”; you’re a mix of chromosomes in a smoothie of chance, your parents, luck, and your agreements or disagreements. Don’t change that. Play, fulfill yourself, and don’t overrate the influence of ego.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Creativity, in 99% of cases, is just the excretions of unhinged people. Don’t glorify art.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              There is nothing more valuable than the act of creation. Art is the thing you must climb the altar for, with everything in your pockets, your heart, and in the “forbidden” folder.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Death can become the center of your thoughts, turning you into a vortex swirling around nothingness, endlessly passing yourself through it. You know nothing about death, and this is the only thing worth accepting.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Individuality is nonsense. That’s how our species works; we look at each other, listen to each other, and are obsessed with our own kind. If it weren’t for the God of Chaos, we would have long since become indistinguishable from each other. Don’t stress over it.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              If you’re going to lie, do it extravagantly, in full, with richness. Petty, lousy half-lies lead to dementia and early senility.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Gratitude turns a rebel into a noble, disarming rascal with a purity in their sociopathic motives.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Gratitude — genuine, ringing, and clear — is the ultimate secret weapon.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              The more people and events you are grateful for, the better you cope with everything. Praises to all that is.
+            </li>
+            <div className="pl-5 -mb-6 lg:pl-[23px] lg:text-[54px] lg:mb-0 lg:mt-16">
+              EXTREME ZONE...
+            </div>
+            <li className="pl-5 lg:pl-[23px]">
+              If you've already done/thought/eaten something — it’s right.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Love and kindness are the hardest things to reconcile with social adaptation.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Love and kindness are undefeated, blessed, light, and radiant.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Love and kindness require the greatest audacity.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Restraining evil is also restraining good.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Avoiding ugliness is also avoiding beauty. In dual systems, action applied to one pole automatically applies to the other, like a lever.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              From the perspective of perception, there is you and, as it were, everything else; when you die, it all ends. You are not the cause of the world, but you are remarkable from beginning to end.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Seek those and that which represent experiences you could never have for whatever reason, and they will come. Share and accept generously in these circumstances.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Love and kindness in others may evoke fear, disgust, pain, or the urge to run.
+              <br />
+              If you feel unpleasant things when encountering love, it’s normal. Receiving and expressing these qualities requires learning and readiness.
+            </li>
+            <li className="pl-5 lg:pl-[23px]">
+              Guard your heart; after weathering a thousand storms in armor, one day, bare and exhausted, you will realize under the sun that every meeting—an extended hand, a greeting, a blessing—accomplishes much more than any attack ever could.
+              <br />
+              Inside out, upside down, backwards, and all awry—go forth, flipped and flung every which way.
+            </li>
+          </ol>
         </div>
-        <li className="pl-5 lg:pl-[23px]">
-          If you've already done/thought/eaten something — it’s right.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Love and kindness are the hardest things to reconcile with social adaptation.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Love and kindness are undefeated, blessed, light, and radiant.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Love and kindness require the greatest audacity.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Restraining evil is also restraining good.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Avoiding ugliness is also avoiding beauty. In dual systems, action applied to one pole automatically applies to the other, like a lever.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          From the perspective of perception, there is you and, as it were, everything else; when you die, it all ends. You are not the cause of the world, but you are remarkable from beginning to end.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Seek those and that which represent experiences you could never have for whatever reason, and they will come. Share and accept generously in these circumstances.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Love and kindness in others may evoke fear, disgust, pain, or the urge to run.
-          <br />
-          If you feel unpleasant things when encountering love, it’s normal. Receiving and expressing these qualities requires learning and readiness.
-        </li>
-        <li className="pl-5 lg:pl-[23px]">
-          Guard your heart; after weathering a thousand storms in armor, one day, bare and exhausted, you will realize under the sun that every meeting—an extended hand, a greeting, a blessing—accomplishes much more than any attack ever could.
-          <br />
-          Inside out, upside down, backwards, and all awry—go forth, flipped and flung every which way.
-        </li>
-      </ol>
+      </div>
     </div>
   );
 };
