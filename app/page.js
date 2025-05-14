06ec8e3d86832f58b43d3971abb51ad434954248
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2025-05-14 21:51:01 +0900

    Fixed homepage

--- app/page.js
+++ app/page.js
@@ -155,7 +155,7 @@ export default function Home() {
       </div>
 
       <div className="hidden lg:block overflow-hidden text-[30px] 2xl:text-[36px]">
-        <div className="relative aspect-[1843/2888] flex flex-col pb-24 2xl:pb-[210px]">
+        <div className="relative w-full min-h-[1800px] aspect-[1843/2888] flex flex-col pb-24 2xl:pb-[210px]">
           <div className="pt-[412px] mx-[26px] ml-auto w-[600px]">
             "If you’re going to lie, do it extravagantly, in full, with richness. Petty, lousy half-lies lead to
             dementia and early senility."
@@ -186,12 +186,12 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="pl-[38px] relative lg:h-[1800px] min-[76rem]:h-[1900px] xl:h-[2000px] min-[92rem]:h-[2100px] 2xl:h-[2264px] overflow-hidden flex flex-col">
+        <div className="pl-[38px] relative min-h-[1400px] overflow-hidden flex flex-col">
           <div className="w-1/2 ml-auto mr-20">
             The dyeing process is overseen by a resident textile artist, ensuring depth of color, oxidation, and
             organic evolution.
           </div>
-          <div className="mt-auto mb-[260px] 2xl:mb-[68px]">
+          <div className="mt-[90%] xl:mt-[95%]">
             <div>Nazvanie Pugovici, Pash</div>
             <div className="w-1/2 mt-[90px]">
               Artifacts embedded in the material-bone buttons hand-carved by a dedicated ivory craftsman, fossilized
@@ -200,29 +200,29 @@ export default function Home() {
             </div>
           </div>
           <div className="absolute left-0 top-0 w-full overflow-hidden -z-10 ml-5">
-            <div className="relative overflow-hidden">
-              <img src="/catenatia-spinosa.webp" alt="Catenatia Spinosa" className="object-contain" />
+            <div className="relative w-full aspect-[1889/2264] overflow-hidden">
+              <Image src="/catenatia-spinosa.webp" alt="Catenatia Spinosa" className="object-cover" fill />
             </div>
           </div>
         </div>
-        <div className="relative w-full lg:h-[2900px] xl:h-[3100px] 2xl:h-[4420px] overflow-hidden flex flex-col">
-          <div className="w-[755px] mx-10 ml-auto mt-24 2xl:mt-[457px]">
+        <div className="relative w-full min-h-[2550px] aspect-[1876/4361] overflow-hidden flex flex-col">
+          <div className="w-[650px] 2xl:w-[755px] mx-10 ml-auto mt-[20%]">
             The boudoir as a battleground—fetishistic silk corsetry, sculptural lingerie, and pieces that merge
             historical dressmaking with radical contemporary expression.
           </div>
-          <div className="w-[759px] ml-[159px] mt-[1190px]">
+          <div className="w-[600px] 2xl:w-[759px] ml-[159px] mt-[52%] min-[70rem]:mt-[55%] xl:mt-[60%]">
             Menswear tailoring redefined—coats, architectural suits, and reimagined tailoring techniques challenge
             traditional boundaries of masculine and feminine form.
           </div>
-          <div className="w-[885px] ml-[662px] mt-[253px]">
+          <div className="w-[700px] 2xl:w-[885px] ml-[30%] 2xl:ml-[40%] mt-[5%]">
             Textile sorcery—fabrics infused with tannins from seasonal chestnut and poppy, harvested in Georgia at the
             precise moment of the collection’s development.
           </div>
-          <div className="w-[888px] ml-[662px] mt-[47px]">
+          <div className="w-[700px] 2xl:w-[888px] ml-[30%] 2xl:ml-[40%] mt-[47px]">
             The dyeing process is overseen by a resident textile artist, ensuring depth of color, oxidation, and
             organic evolution.
           </div>
-          <div className="w-[840px] mt-auto ml-10">
+          <div className="w-1/2 2xl:w-[840px] mt-auto ml-10">
             The garments exist not in isolation but within a rich tapestry
             of literary narratives, visual storytelling, and curated artistic statements.
             <br /><br />
@@ -231,8 +231,8 @@ export default function Home() {
             an unfolding dialogue between textile, philosophy, and the body.
           </div>
           <div className="absolute left-0 top-0 w-full overflow-hidden -z-10 px-9">
-            <div className="relative w-full overflow-hidden">
-              <img src="/vincula-eprantium.webp" alt="Vincula Eprantium" className="object-contain" />
+            <div className="relative w-full aspect-[1876/4361] overflow-hidden">
+              <Image src="/vincula-eprantium.webp" alt="Vincula Eprantium" className="object-cover" fill />
             </div>
           </div>
         </div>
@@ -242,7 +242,7 @@ export default function Home() {
           </div>
           <Button text="WATCH COLLECTION" href="/collection" />
         </div>
-        <div className="relative w-full h-[2900px] flex flex-col">
+        <div className="relative w-full aspect-[2014/3227] flex flex-col">
           <div className="w-[793px] mx-10">
             Odiosa is an artisanal, multidisciplinary phenomenon,
             operating through the lens of performance, documentary,
@@ -251,12 +251,12 @@ export default function Home() {
             A dedicated expedition team sources the finest Italian silks, batistes, crepes, modals, chiffons, and
             natural fiber blends, ensuring absolute quality across the brand’s vast production landscape.
           </div>
-          <div className="w-[910px] mt-auto mb-16 ml-auto mr-10">
+          <div className="w-[730px] 2xl:w-[910px] mt-auto mb-16 min-[110rem]:mb-40 ml-auto mr-10">
             Each piece is meticulously documented through an ongoing film project, offering an intimate, voyeuristic
             insight into the ateliers, capturing the raw process of creation as a living, breathing entity.
           </div>
-          <div className="absolute left-0 -top-[420px] w-full h-[3927px] overflow-hidden -z-10">
-            <div className="relative w-full h-[3927px] overflow-hidden">
+          <div className="absolute left-0 -top-[420px] w-full overflow-hidden -z-10">
+            <div className="relative w-full aspect-[2014/4347] overflow-hidden">
               <Image
                 src="/malum-arsum.webp"
                 alt="Malum Arsum.webp"
